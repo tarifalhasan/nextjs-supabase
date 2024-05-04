c99fe2bbdaf90ad9be9fdc3e07e71cc0c06f18fd
tarifalhasan <tarifalhasan999@gmail.com>
2024-05-04 13:55:21 +0600

    auth with clint compomnent

---
+++ lib/supabase.ts
@@ -0,0 +1,13 @@
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { createClient } from "@supabase/supabase-js";
+export const supabaseAdmin = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SURVICE_ROLE_KEY!
+);
+
+// export const supabase = createClient(
+//   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+// );
+
+export const supabase = createClientComponentClient();
